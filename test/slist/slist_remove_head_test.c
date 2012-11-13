b4063a7419fe03fd2a6380482e581ccbd143cccc
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-11-12 20:46:45 -0800

    [slist] added remove head test
    * added test for removing head
    * fixed a test comment in after_insert_test

--- test/slist/slist_remove_head_test.c
+++ test/slist/slist_remove_head_test.c
@@ -3,29 +3,28 @@
 #include "slist_test_element.h"
 
 
-void steel_slist_after_insert_test(){
+void steel_slist_remove_head_test(){
   steel_slist_test_element_t my_element1;
   steel_slist_test_element_t my_element2;
-  steel_slist_test_element_t my_element3;
-  steel_slist_test_element_t* second_elem;
+  steel_slist_test_element_t *removed_head;
   steel_slist_t my_list;
 
   steel_slist_init(&my_list, sizeof(steel_slist_test_element_t), offsetof(steel_slist_test_element_t, link));
   my_element1.value = 5;
   my_element2.value = 1;
-  my_element3.value = 9;
   steel_slist_insert_tail(&my_list, &my_element1);
   steel_slist_insert_tail(&my_list, &my_element2);
-  steel_slist_insert_after(&my_list, &my_element1, &my_element3);
-  second_elem = (steel_slist_test_element_t*)steel_slist_next(&my_list, &my_element1);
+
+  removed_head = (steel_slist_test_element_t*)steel_slist_remove_head(&my_list);
 
 
-  ASSERT_TRUE(&my_element3 == second_elem, "Second element doesn't equal the third element.")
+  ASSERT_TRUE(&my_element1 == removed_head, "Removed Head doesn't equal the first element.")
+  ASSERT_TRUE(&my_element2 == (steel_slist_test_element_t*)steel_slist_head(&my_list), "New Head doesn't equal the second element.")
 }
 
 
 int main(int argc, char *argv[]){
-  steel_slist_after_insert_test();
+  steel_slist_remove_head_test();
   return 0;
 }
 
